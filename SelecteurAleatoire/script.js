105af5f4c8a8b36e936583263c68e44a872472d9
Freench <f.pernot@codeur.online>
2021-09-01 14:12:18 +0200

    branch todolistbranch

---
+++ SelecteurAleatoire/script.js
@@ -0,0 +1,7 @@
+let notPassed = ["José", "Tommy", "Francis"];
+let Passed = [];
+btnChoisir = document.getElementById("btn-choisir");
+
+btnChoisir.addeventlistener("click", function(){
+    console.log(click)
+});
