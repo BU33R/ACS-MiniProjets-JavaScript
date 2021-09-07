e4a5b119b385ef62b31df5d756d13c1652e16642
Freench <f.pernot@codeur.online>
2021-09-07 14:53:07 +0200

    ça marche

--- SelecteurAleatoire/script.js
+++ SelecteurAleatoire/script.js
@@ -2,11 +2,13 @@
 class apprenant{
     constructor(name, photo, passed){
         this.name = name;
-        this.photo = photo;
+        if(photos[name]){
+            this.photo = photos[name]
+        }
         this.passed = passed;
         this.elementHtml = createElementHtml();
-
-
+        this.elementHtml.src= this.photo
+        console.log(this.photo)
         this.areaGuyChoiced = document.getElementById('div-personne-choisi')
         this.areaNotPassed = document.getElementById('lapin')
         this.areaPassed = document.getElementById('personne-passee')
@@ -20,7 +22,6 @@ class apprenant{
         this.posLeft = 0;
         this.setPosition
     }
-
 
     getPosition(){
         return [this.elementHtml.offsetTop, this.elementHtml.offsetLeft]
@@ -41,7 +42,6 @@ class apprenant{
         copy_guy = this.elementHtml.cloneNode(true)
         copy_guy.style.opacity = '0'
         areaTarget.appendChild(copy_guy)
-        console.log("Avant ", this.elementHtml.offsetLeft, this.elementHtml.offsetTop, "Après ", copy_guy.offsetLeft, copy_guy.offsetTop  )
         this.setPosition(0, 0, true)
         this.elementHtml.style.position = 'absolute';
         this.elementHtml.style.top = this.elementHtml.offsetTop + 'px';
@@ -55,8 +55,7 @@ class apprenant{
             this.elementHtml.style.position = 'static';
 
             if (verfiLast){
-                console.log("on verifie")
-                verifingLastPassage()
+                // verifingLastPassage()
                 btnChoisir.disabled = false
             }else{
                 btnChoisir.disabled = false
@@ -83,7 +82,8 @@ let passed = []
 
 //Data
 let images=["icon.svg", "icon.svg", "icon.svg", "icon.svg","icon.svg", "icon.svg", "icon.svg", "icon.svg","icon.svg", "icon.svg", "icon.svg", "icon.svg" ]
-let photos = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+let photos = {Francis: "images/francis.png", Tommy:"icon.svg", Joselito:"icon.svg", Alphonso:"icon.svg", Dylan:"icon.svg", Yamina:"icon.svg",
+              Laurent:"icon.svg", Arthur:"icon.svg", Céleste:"icon.svg", Lucas:"icon.svg", Aleadin:"icon.svg", Marley:"icon.svg"}
 
 //Boutons
 let btnChoisir = document.getElementById("btn-choisir")
@@ -95,25 +95,22 @@ let areaGuyChoiced = document.getElementById('div-personne-choisi')
 let areaNotPassed = document.getElementById('lapin')
 let areaPassed = document.getElementById('personne-passee')
 
-// if(!localStorage.getItem("first")){
-//     listNotPassed = ["Tommy", "Joselito", "Francis", "Alphonso", "Dylan", "Yamina", "Laurent", "Arthur", "Céleste", "Lucas", "Aleadin", "Marley"]
-//     listPassed = []
-// }else{
-//     listNotPassed = localStorage.getItem("notPassed").split(',')
-//     listPassed = localStorage.getItem("Passed").split(',')
-// }
+if(!localStorage.getItem("first")){
+    listNotPassed = ["Tommy", "Joselito", "Francis", "Alphonso", "Dylan", "Yamina", "Laurent", "Arthur", "Céleste", "Lucas", "Aleadin", "Marley"]
+    listPassed = []
+
+}else{
+    listNotPassed = localStorage.getItem("notPassed").split(',')
+    listPassed = clearList(localStorage.getItem("passed").split(','))
+}
 
 for(i=0; i<listNotPassed.length; i++){
     notPassed.push(new apprenant(listNotPassed[i], photos[i] , false))
 }
-
-
 for(i=0; i<listPassed.length; i++){
     passed.push(new apprenant(listPassed[i], photos[i], true))
 }
-
 affiche();
-
 
 btnChoisir.addEventListener("click", function(){
     btnChoisir.disabled = true;
@@ -122,77 +119,70 @@ btnChoisir.addEventListener("click", function(){
         passed.push(onBoard[0]);
         onBoard = [];
     }
-    choisirGuy();
+    choiceGuy();
+    if(notPassed.length == 0){
+        onBoard[0].deplacementTo(areaPassed, true);
+        passed.push(onBoard[0]);
+        onBoard = [];
+        setTimeout(() => {
+        verifingLastPassage();
+    }, 500);
+    }
     affiche();
 })
 
-// btnOk.addEventListener("click", function(){
-//     len = areaGuyChoiced.children.length
-//     console.log(len)
-//     for(i = 0; i < len; i++){
-//         deplacement(areaGuyChoiced.children[0], areaPassed, true)
-//     }
-// });
-
 function verifingLastPassage(){
     if (!notPassed.length){
-        // notPassed = passed
-        // passed = []
-        // console.log("Enfant passées : ", areaPassed.children.length)
-        let fin = areaPassed.children.length;
-        while(passed != []){
+        while(passed.length > 0){
             passed[0].deplacementTo(areaNotPassed)
             notPassed.push(passed[0])
             passed.shift()
         }
-        // for(i = 0; i < areaNotPassed.children.length; i++){
-        //     console.log("on est là")
-        //     areaNotPassed.children[i].style.top =  areaNotPassed.children[i].offsetTop + 'px';
-        //     areaNotPassed.children[i].style.left =  areaNotPassed.children[i].offsetLeft + 'px';
-        // }
         affiche();
     }
 }
 
 function affiche(){
     pNotPassed.innerHTML = ""
+    listNotPassed = []
+    listPassed = []
     for(np of notPassed){
         pNotPassed.innerHTML += np.name + " <br>"
+        listNotPassed.push(np.name)
     }
     pPassed.innerHTML = ""
     for(p of passed){
         pPassed.innerHTML += p.name + " <br>"
+        listPassed.push(p.name)
+    }
+    if(onBoard.length>0){
+        listPassed.push(onBoard[0].name)
     }
 
-    // pNotPassed.innerHTML = notPassed.join(" <br>")
-    // window.localStorage.setItem("notPassed", notPassed)
-    // window.localStorage.setItem("Passed", passed)
-    // window.localStorage.setItem("first", false)
+    //Sauvegarde
+    localStorage.setItem( 'notPassed', listNotPassed);
+    localStorage.setItem( 'passed', listPassed);
+    window.localStorage.setItem("first", false)
 }
 
-function choisirGuy(){
+function choiceGuy(){
     if(notPassed.length == 1){
         idChoice = 0;
     }else{
         idChoice = Math.floor(Math.random()*(notPassed.length));
     };
     choice = notPassed[idChoice]
-    // console.log("Le choix est ", idChoice, choice)
     if (choice == ""){
         console.log("Le problème !!")
         notPassed.splice(idChoice, 1)
-        choisirGuy()
+        choiceGuy()
     }else{
         notPassed.splice(idChoice, 1)
         pGuyChoiced.innerHTML = choice.name
-        console.log("choice : ", typeof choice, choice)
         choice.deplacementTo(areaGuyChoiced)
         onBoard.push(choice)
     }
 }
-
-
-
 
 function createElementHtml(){
     elementHtml = document.createElement('img');
@@ -231,4 +221,17 @@ function idGenerator(){
     idGenerated ++;
     return 'id-' + idGenerated
 }
+
+function clearList(liste){
+    len = liste.length
+    let new_list = []
+    for(i=0; i<len; i++){
+        if(liste[i] != "" && liste[i] != ''){
+            new_list.push(liste[i])
+        }
+    }
+    return new_list
+}
+
+
 
