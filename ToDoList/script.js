e3537232babd777fcb693cd572876ff835d4afe4
BU33R <t.laubepin@codeur.online>
2021-09-08 14:11:19 +0200

    Ajout des commentaires

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,35 +1,45 @@
-const list = document.getElementById('list');
-const form = document.querySelector('form');
-const item = document.getElementById('item');
+const list = document.getElementById('list'); //___________selecteur________________
+const form = document.querySelector('form');  //_________Valeur d'input_____________
+const item = document.getElementById('item'); //_____________RENDU__________________
 
 // ajouter un élément
 form.addEventListener('submit', (e) => {
-  e.preventDefault(); // indique à l'agent utilisateur que si l'évènement n'est pas explicitement géré, l'action par défaut ne devrait pas être exécutée comme elle l'est normalement.
+  // pour se prévaloir d'un comportement, qui fesait que, ce qui etait taper dans l'input ne restais pas afficher dans la listes
+  e.preventDefault();
+  // création d'elements et valeur rentrer dans l'input  
   list.innerHTML += `<li>${item.value}</li>`;
+  // A chaque ajout, on stock dans le localStorage
   storage();
+  //Input repasse a zéro 
   item.value = "";
 });
 
-// supprimer l'élément
+// suppression d'éléments
 list.addEventListener('click', (e) => {
+  // SI un element dans la liste contient'CHECKED', alors supprime
   if (e.target.classList.contains('checked')) {
     e.target.remove();
+    //SINON Le mettre en 'CHECKED'
   } else {
     e.target.classList.add('checked');
   }
+  //Actualisation du stockage quand supprimer
   storage();
 });
 
 // partie de stockage
 function storage() {
+  //localStorage du nom de todoList qui contiendras tout les todos
   window.localStorage.todoList = list.innerHTML;
 }
-
+//Obtenir les valeurs
 function getValues() {
   let storageContent = window.localStorage.todoList;
+  //SI storageContent n'existe pas ou si il n'y a rien dedans  
   if (!storageContent) {
     list.innerHTML =
       `<li>Cliquez sur une tâche pour la supprimer</li>`;
+    //
   } else {
     list.innerHTML = storageContent;
   }
